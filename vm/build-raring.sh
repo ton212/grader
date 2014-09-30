b9336d6e7590b024f25c65e08ebbf59610dba53a
PhompAng <tingtong003tomy@gmail.com>
2014-09-30 11:42:40 +0700

    delete unnecessary access and error log configuration from nginx.conf

--- vm/build-raring.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Build trusty image
-# http://docs.docker.io/en/latest/use/baseimages/
-sudo debootstrap trusty trusty http://mirror1.ku.ac.th/ubuntu/
-sudo tar -C trusty -c . | sudo docker import - trusty
